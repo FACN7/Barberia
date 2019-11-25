743dd5baf8aa924569a05f42bb87ed5beab735e8
nataphilips <fnd.natalia@gmail.com>
2019-11-25 14:30:45 +0200

    Installed and imported Calendar and Time Picker components Relates #9 and #10

--- barberia/src/App/pages/Home.js
+++ barberia/src/App/pages/Home.js
@@ -1,19 +1,19 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class Home extends Component {
   render() {
     return (
-    <div className="App">
-      <h1>Project Home</h1>
-      {/* Link to List.js */}
-      <Link to={'./list'}>
-        <button variant="raised">
-            My List
-        </button>
-      </Link>
-    </div>
+      <div className="App">
+        <h1>Project Home</h1>
+        {/* Link to List.js */}
+        <Link to={"./list"}>
+          <button variant="raised">My List</button>
+        </Link>
+        <Link to={"./calendar"}>
+          <button variant="raised">Calendar</button>
+        </Link>
+      </div>
     );
   }
 }
---
+++ barberia/src/App/pages/TimePicker.js
@@ -0,0 +1,54 @@
+import React from "react";
+import AppointmentPicker from "appointment-picker";
+import "../../../node_modules/appointment-picker/dist/appointment-picker.css";
+
+class AppoPicker extends React.Component {
+  constructor(props) {
+    super(props);
+    this.options = {
+      leadingZero: true,
+      interval: 30,
+      mode: "12h",
+      minTime: 10,
+      maxTime: 21,
+      startTime: 9,
+      endTime: 23,
+      disabled: ["1:30 pm", "2:00 pm", "7:30 pm"]
+    };
+    this.state = { time: {} };
+    this.pickerRef = React.createRef();
+    this.onTimeSelect = this.onTimeSelect.bind(this);
+  }
+
+  onTimeSelect(event) {
+    console.log("change.appo.picker", event.time);
+    this.setState({ time: event.time });
+  }
+
+  render() {
+    return (
+      <div>
+        <input type="text" ref={this.pickerRef}></input>
+        {/* <code>{JSON.stringify(this.state.time)}</code>*/}
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    this.picker = new AppointmentPicker(this.pickerRef.current, this.options);
+    this.pickerRef.current.addEventListener(
+      "change.appo.picker",
+      this.onTimeSelect
+    );
+  }
+
+  componentWillUnmount() {
+    this.pickerRef.current.removeEventListener(
+      "change.appo.picker",
+      this.onTimeSelect
+    );
+    this.picker.destroy();
+  }
+}
+
+export default AppoPicker;
