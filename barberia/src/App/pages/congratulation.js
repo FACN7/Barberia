477eb2b38936d9f7213ce51ca88908cce9799c31
mohamadD <rediab88@gmail.com>
2019-11-26 15:43:08 +0200

    congratulations page, uses data from local storage. to be changed later (if there's time)

---
+++ barberia/src/App/pages/congratulation.js
@@ -0,0 +1,25 @@
+import React, { Component, useState } from "react";
+import "./congratulation.css";
+
+function congratulation() {
+  const userData = JSON.parse(localStorage.getItem("confirmationData"));
+  const { email, phone } = userData;
+  console.log(email, phone);
+
+  return (
+    <div>
+      <div className="ms_header">
+        <h3 className="text_header">
+          Congratulation, your appointment is booked at Nazareth, El-ein We sent
+          the confirmation to {email} and {phone}
+        </h3>
+      </div>
+      <form>
+        <input type="submit" className="gohome_button" value="HOME" />
+        <input type="submit" className="cancel_button" value="Cancel" />
+      </form>
+    </div>
+  );
+}
+
+export default congratulation;
