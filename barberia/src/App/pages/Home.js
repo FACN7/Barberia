13d25df14349bc5e5fe628d65fc4f80704a77876
okarem <karem.omary@gmail.com>
2019-11-28 17:07:10 +0200

    added link to google maps in the front page

--- barberia/src/App/pages/Home.js
+++ barberia/src/App/pages/Home.js
@@ -2,12 +2,20 @@ import React from "react";
 import { HotShitButton, YellowButton } from "../components/buttons";
 import "../App.css"
 import "./Home.css"
+import {A} from "hookrouter";
 const Home = () => {
   return (
     <div className="App">
       <div className="barberia-name"><h3>HOSAM'S BARBERIA</h3></div>
       <HotShitButton href='/calendar' title="40 shekel" description="Quick Haircut Booking" />
 
+
+      <footer>
+        
+        <div className="location-button">
+        <a href="https://goo.gl/maps/cZ7B3m6ev6H9xTEt8"><img src="https://img.icons8.com/pastel-glyph/64/000000/place-marker.png" /> NAZARETH, EL-EIN</a>
+          </div>
+      </footer>
     </div>
   );
 }
