013d245dcb837499c63a5496f537c25ff1ccc621
mohamadD <rediab88@gmail.com>
2019-12-02 10:10:14 +0200

    fix sql injection relate #60

--- src/queries/cancelbooking.js
+++ src/queries/cancelbooking.js
@@ -3,7 +3,8 @@ const databaseConnection = require("../database/db_connection.js");
 const cancelbooking = rawData => {
   const data = rawData;
   return databaseConnection.query(
-    `DELETE FROM bookings WHERE (booking_date='${data.date}' AND booking_time='${data.time}' AND customer_name='${data.name}')`
+    `DELETE FROM bookings WHERE (booking_date=$1 AND booking_time=$2 AND customer_name=$3)`,
+    [data.date, data.time, data.name]
   );
 };
 
--- src/queries/getUnavailableTimes.js
+++ src/queries/getUnavailableTimes.js
@@ -2,7 +2,8 @@ const databaseConnection = require("../database/db_connection.js");
 
 const getBusyTimeSlots = baseDate => {
   return databaseConnection.query(
-    `SELECT booking_time FROM bookings WHERE booking_date='${baseDate}'`
+    `SELECT booking_time FROM bookings WHERE booking_date=$1`,
+    [baseDate]
   );
 };
 
