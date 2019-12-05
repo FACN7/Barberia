9cb91d0ea4634563fe42d8e1e78567168ae88f7e
okarem <karem.omary@gmail.com>
2019-12-06 00:43:20 +0200

    burgerbar is finished and added to relevant pages

--- barberia/src/App/pages/AdminWorkingDays.js
+++ barberia/src/App/pages/AdminWorkingDays.js
@@ -1,17 +1,23 @@
 import React from "react";
+import { BurgerBar } from "../components/BurgerBar"
 import IntervalViewer from "../components/IntervalViewer";
 function AdminWorkingDays() {
 
   return (
-    <div className="App">
-      <IntervalViewer day="0" />
-      <IntervalViewer day="1" />
-      <IntervalViewer day="2" />
-      <IntervalViewer day="3" />
-      <IntervalViewer day="4" />
-      <IntervalViewer day="5" />
-      <IntervalViewer day="6" />
-    </div>
+    <div>
+      <BurgerBar>
+        <h3><a href="/admin">Check appointments</a></h3>
+        <h3><a href="/adminworkingdays">Change working hours</a></h3>
+      </BurgerBar>
+      <div className="App">
+        <IntervalViewer day="0" />
+        <IntervalViewer day="1" />
+        <IntervalViewer day="2" />
+        <IntervalViewer day="3" />
+        <IntervalViewer day="4" />
+        <IntervalViewer day="5" />
+        <IntervalViewer day="6" />
+      </div></div>
   );
 }
 
